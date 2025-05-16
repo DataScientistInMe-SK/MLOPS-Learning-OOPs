f2cc96969cc33883f03c7c776fd72142cb0d16c5
DataScientistInMe-SK <datascientistinme@gmail.com>
2025-05-16 12:46:48 +0530

    C1. Class and object explanation with easy to understand exmaples

---
+++ F1-Oops.py
@@ -0,0 +1,28 @@
+# Step1: To initiate a class
+class employee:
+    # There are some special methods/ magic methods or dunder methods, AKA Constructors.
+    def __init__(self):
+        # print("Self executed data or attributes- Start")
+        self.id = 10001
+        self.age = 32
+        self.salary = 200000
+        self.designation = "Data Scientist"
+        # print("Self executed data or attributes- Done")
+    
+    def travel(self, destination):
+        print("Manually executed functions or methods")
+        print(f"Employee is now eligible to travel to {destination}")
+        
+        
+        
+# Step2: Create an object or instance of the class
+John = employee()
+
+## Calling Default features i.e., Attributes
+# print("Age:",John.age)
+# print("ID:",John.id)
+
+## Calling functionalty based features i.e., Methods
+# John.travel("Frankfurt")
+
+print(type(John))
---
+++ F2-RoughWork.py
@@ -0,0 +1,14 @@
+# Program to check class of various types of data
+
+my_list = [1,2,3,4]
+my_string = " Hawaii"
+my_number = 10001
+
+# print("List Type:", type(my_list))
+# print("String Type:", type(my_string))
+# print("NUmber Type:", type(my_number))
+
+# my_list.clear()
+# print("List :", my_list)
+# my_list.capitalize()
+print(my_string.capitalize())
